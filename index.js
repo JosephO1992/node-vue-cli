635cb3a86dfd169e8b1310bbecf5edc921c550d0
JosephO1992 <josephraoreilly@gmail.com>
2022-02-27 18:51:38 +0000

    installed yargs to make my life easier:

--- index.js
+++ index.js
@@ -1,65 +1,46 @@
 #! /usr/bin/env node
 
-// add the page with the .vue extension
-
 const fs = require("fs");
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
   
-// const path = "./new-Directory"
-
-const args = process.argv.slice(2);
+const path = "./src"
 const typePassed = process.argv[0]
 const types = ['component', 'view', 'route', 'store']
 
-let cont = false;
-
-//check the first flag is create
-function checkFlag(something) {
-    let splitThem = something[0].split(':')
-    if(splitThem[0] !== 'create') {
-       return console.error('command must start with create:<something> use --help for more options')
+const createFile = () => {fs.access(path, (error) => {
+    // To check if the given directory 
+    // already exists or not
+    if (error) {
+      // If current directory does not exist
+      // then create it
+      fs.mkdir(path, (error) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("New Directory created successfully !!");
+        }
+      });
     } else {
-        return console.log(`now creating ${splitThem[1]} ...`)
+      console.log("Given Directory already exists !!");
     }
+  });
 }
 
-checkFlag(args);
+if(argv.create === 'component') {
+    console.log('component selected')
+} 
 
-// get whether its a component/view/route/store
-function getType(something) {
-    let splitThem = something[0].split(':')
-    return splitThem[1];
+if(argv.create === 'view') {
+    console.log('view selected')
 }
 
-types.forEach((type) => {
-    if(type === typePassed) {
-        cont = true
-    }
-})
-  
-// fs.access(path, (error) => {
-//   // To check if the given directory 
-//   // already exists or not
-//   if (error) {
-//     // If current directory does not exist
-//     // then create it
-//     fs.mkdir(path, (error) => {
-//       if (error) {
-//         console.log(error);
-//       } else {
-//         console.log("New Directory created successfully !!");
-//       }
-//     });
-//   } else {
-//     console.log("Given Directory already exists !!");
-//   }
-// });
+if(argv.create === 'route') {
+    console.log('route selected')
+} 
 
-// if(cont) {
-//     console.log(`${typePassed} will now be created`)
-// } else {
-//     console.log('bad luck pal')
-// }
+if(argv.create === 'store') {
+    createFile()
+}
 
-// console.log(args[0].split(':'))
-
-console.log(getType(args))
